6138901d60a43858b43ca62b67ae021bb7d0614a
Somov Evgeniy <evgeniy.somov@ilogos.biz>
2020-01-24 13:46:48 +0300

    Добавлено ожидание sleep

--- Test1.py
+++ Test1.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 driver = webdriver.Chrome(executable_path="C:\GIT\Template-Selenium-Python\webdriver\chromedriver.exe")
 
@@ -8,6 +9,8 @@ driver.get("https://www.google.com/")
 search = driver.find_element_by_name("q")
 search.send_keys("GeForce 1650")
 search.send_keys(Keys.ENTER)
+
+time.sleep(5)
 
 driver.close()
 driver.quit()
